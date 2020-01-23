8b43081fa13a624ef35d9496a943b8ab580cb83d
Roy Segall <roy@segall.io>
2020-01-23 23:10:34 +0200

    Adding some base files

---
+++ management/commands/tivol.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        print('hello!')
