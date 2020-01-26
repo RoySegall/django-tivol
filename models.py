b3e321568a5198fee4899917caa0e47e06cd0797
Roy Segall <roy@segall.io>
2020-01-26 06:35:54 +0200

    Adding model for tracking migrated rows.

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ContentMigrationStatus(models.Model):
+    """
+    Keep tracking of rows from sources and to where they were migrated. This
+    will help us rollback any migrated content or to know if a content has been
+    migrated or not.
+    """
+    source_id = models.CharField(max_length=255)
+    destination_id = models.IntegerField()
+    model_target = models.CharField(max_length=255)
