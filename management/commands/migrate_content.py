8bd25e3d02556ab18f4cb0018a95fb73feb1391c
Roy Segall <roy@segall.io>
2020-01-27 15:46:55 +0200

    add todo.

--- management/commands/migrate_content.py
+++ management/commands/migrate_content.py
@@ -11,22 +11,23 @@ class Command(BaseCommand, SwagHelpers):
     help = 'Migrating data into the system'
 
     def handle(self, *args, **options):
+        # todo: move to swag.
         io = ConsoleIO()
 
         # Get the entry point instance.
         entry_point = self.instance_entrypoint()
+
+        # todo: move to swag.
         bar = ProgressBar(io, len(entry_point.migration_handlers))
         bar.set_bar_character("■")
         bar.set_progress_character('')
 
-        self.green("Start to migrate \n")
+        self.green("Start to migrate")
         bar.start()
-
         for migration_handler in entry_point.migration_handlers:
             migration = migration_handler()
             bar.advance()
             time.sleep(1)
             self.green(' ' + migration.migrate())
 
-        print("\n")
         self.green('Migrated')
