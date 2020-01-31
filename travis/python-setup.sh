68b3130b0a8c4fdab4877bb6d582a1256c3fef70
Roy Segall <roy@segall.io>
2020-01-31 09:50:45 +0200

    Commit stuff?

--- travis/python-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#pip install -r requirements.txt
-mysql -e 'CREATE DATABASE bifrost;'
-#cp bifrost/local_settings.travis.py bifrost/local_settings.py
-pip install pycodestyle
--- travis/python-unitest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#python manage.py migrate
-#python manage.py test
