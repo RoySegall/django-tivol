5c460d692846d65a853e496633e49f296fcecbc2
Roy Segall <roy@segall.io>
2020-01-31 10:12:19 +0200

    Adding code here

--- tests/settings.py
+++ tests/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoTesting.urls'
+# ROOT_URLCONF = 'djangoTesting.urls'
 
 TEMPLATES = [
     {
