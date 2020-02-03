77840cd527ec3b49970c4ca8b63066c9939a24d9
Roy Segall <roy@dm.office.com>
2020-02-03 21:01:52 +0200

    Start to add the source plugin

--- tivol/base_classes/plugins.py
+++ tivol/base_classes/plugins.py
@@ -31,3 +31,31 @@ class UppercasePlugin(PluginBase):
 
     def process(self, value, extra_info=None):
         return value.capitalize()
+
+
+class ReferencePlugin(PluginBase):
+    """
+    Return an ID of a migrated record based on the ID from the CSV files.
+
+    For example, we need to migrate directors and movies. We also need to keep a relationship between a movie and the
+    director of that movie. Let's look on two CSV files:
+    directors.csv:
+        id, name
+        director_1,Michael Benjamin Bay
+        director_2,Martin Scorsese
+
+    Now, how should movies.csv look like? like that:
+        id, name, director
+        movie_1,The Wolf of Wall Street,director_2
+        movie_2,The Wolf of Wall Street,director_2
+        movie_3,The Departed,director_2
+        movie_4,Pearl Harbor,director_1
+        movie_5,Transformers,director_1
+        movie_5,Transformers 2: Revenge of the Fallen,director_1
+
+    Tivol keeps track of the ID from the source files, CSV, JSON or DB records, and know what is the ID of the record
+    in the DB after the migration process completed.
+    """
+
+    def process(self, value, extra_info=None):
+        return 1
