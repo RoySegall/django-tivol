1f012a48edebc320bb5e9d80b29d961d841f00ef
Roy Segall <roy@segall.io>
2020-02-07 22:03:47 +0200

    Adding tests.

--- tivol/tests/test_migration_handler_base.py
+++ tivol/tests/test_migration_handler_base.py
@@ -89,3 +89,9 @@ class TestMigrationHandlerBase(TransactionTestCase):
         self.migration.migrate()
         self.assertEqual(7, Animal.objects.count())
         Animal.objects.get(animal_name='Cat')
+
+    def test_content_migrate_with_reference(self):
+        """
+        Testing migration when a migration depends on another migration.
+        """
+        pass
--- tivol/tests/test_plugins.py
+++ tivol/tests/test_plugins.py
@@ -5,6 +5,9 @@ from tivol.base_classes.plugins import DatePlugin, UppercasePlugin
 class TestPlugins(TestCase):
 
     def test_date_plugin(self):
+        """
+        Testing the date plugin.
+        """
         date_plugin = DatePlugin()
         parsed = date_plugin.process('April 1, 1976', {'format': '%B %d, %Y'})
         self.assertEqual(parsed.year, 1976)
@@ -12,6 +15,15 @@ class TestPlugins(TestCase):
         self.assertEqual(parsed.day, 1)
 
     def test_uppercase_plugin(self):
+        """
+        Testing the upper case plugin.
+        """
         date_plugin = UppercasePlugin()
         parsed = date_plugin.process('pizza')
         self.assertEqual(parsed, 'Pizza')
+
+    def test_reference_plugin(self):
+        """
+        Testing the reference plugin.
+        """
+        pass
